937a90d0a689ed6ef31c713a4b6aa0abaebc6d30
Manuel Heidrich <manuel.heidrich@perfood.de>
2021-10-18 06:34:27 +0200

    Initial commit

---
+++ functions/get-id.js
@@ -0,0 +1,20 @@
+const fetch = require('node-fetch');
+
+const BOT_TOKEN = process.env.BOT_TOKEN;
+
+function sendMessage(chatId, text) {
+  const url = `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage?chat_id=${chatId}&text=${encodeURIComponent(text)}`;
+
+  return fetch(url);
+}
+
+module.exports.handler = async (event) => {
+  const body = JSON.parse(event.body);
+  console.log(body);
+
+  if (body.my_chat_member) {
+    await sendMessage(body.my_chat_member.chat.id, `This chat's ID is ${body.my_chat_member.chat.id}`);
+  }
+
+  return { statusCode: 200 };
+};
